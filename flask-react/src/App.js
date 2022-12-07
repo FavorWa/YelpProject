ffc7b3f54b2783fc30569e101f94b7b3f834e850
matdav2628 <75546878+matdav2628@users.noreply.github.com>
2022-12-06 21:57:47 -0500

    Add login, logout, profile

--- flask-react/src/App.js
+++ flask-react/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 import { useState } from 'react'
 import axios from "axios";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
+
 
 function App() {
+  return (
+    <main className = "column">
+      <h1>Auth0 Login</h1>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </main>
+  );
+
   const [profileData, setProfileData] = useState(null)
 
   function getData() {
