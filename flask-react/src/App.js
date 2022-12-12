9812b9e81fe96bb103e97d39e2cd5780b71425c9
matdav2628 <75546878+matdav2628@users.noreply.github.com>
2022-12-12 17:59:13 -0500

    App.js changes

--- flask-react/src/App.js
+++ flask-react/src/App.js
@@ -1,16 +1,15 @@
-
 import './App.css';
 import React from 'react';
 import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 import { useState } from 'react'
 import axios from "axios";
+import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
 
 
 
-function YelpSearch() {
-  // Set up state variables to store the user's input and the response from the Yelp API
+function App() {
+  
   const [location, setLocation] = useState('');
   const [term, setTerm] = useState('');
   const [response, setResponse] = useState(null);
@@ -49,16 +48,14 @@ function YelpSearch() {
     });
   }
 
-
-  // Render the form and the response from the Yelp API (if any)
-  return(
-    <div className='App'>
-    <>
-       <LoginButton />
-       <LogoutButton />
-       <Profile />
+  return (
+    <div className="App">
+      <>
+      <LoginButton />
+      <LogoutButton /> 
+      <Profile /> 
      </>
-     <header className="App-header">
+      <header className="App-header">
       <h1>Findit</h1>
       <form onSubmit={handleSubmit}>
         <label>
@@ -70,18 +67,6 @@ function YelpSearch() {
           <input type="text" name="term" value={term} onChange={handleInputChange} />
         </label>
         <button type="submit">Search</button>
-    .then((response) => {
-      const res =response.data
-      setProfileData(({
-        profile_name: res.name,
-        about_me: res.about}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-        }
-    })}
       </form>
       {response &&
         <div>
@@ -96,7 +81,95 @@ function YelpSearch() {
       }
       </header>
     </div>
-    );
+  );
 }
 
-export default YelpSearch;
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import React from 'react';
+// import LoginButton from './components/LoginButton';
+// import { useState } from 'react'
+// import axios from "axios";
+// import LogoutButton from './components/LogoutButton';
+// import Profile from './components/Profile';
+
+
+
+// function App() {
+  
+    
+//   const [profileData, setProfileData] = useState(null)
+
+  
+//   function getData() {
+//     axios({
+//       method: "GET",
+//       url:"/profile",
+//     })
+//     .then((response) => {
+//       const res =response.data
+//       setProfileData(({
+//         profile_name: res.name,
+//         about_me: res.about}))
+//     }).catch((error) => {
+//       if (error.response) {
+//         console.log(error.response)
+//         console.log(error.response.status)
+//         console.log(error.response.headers)
+//         }
+//     })}
+  
+//   return (
+//     <div className="App">
+//       <>
+//       <LoginButton />
+//       <LogoutButton /> 
+//       <Profile /> 
+//      </>
+//       <header className="App-header">
+//       <h1>Findit</h1>
+//       <form>
+//       <fieldset>
+//          <label>
+//            <p>Keyword</p>
+//            <input keyword="keyword"/>
+//          </label>
+//          <label>
+//            <p>City</p>
+//            <input city="city"/>
+//          </label>
+//          <label>
+//            <p>Radius</p>
+//            <input radius="radius"/>
+//          </label>
+//          <label>
+//            <p>Limit</p>
+//            <input limit="limit"/>
+//          </label>
+//        </fieldset>
+//        <button type="submit">Submit</button>
+//       </form>
+//         <p> </p><button onClick={getData}>Click me</button>
+//         {profileData && <div>
+//               <p>Profile name: {profileData.profile_name}</p>
+//               <p>About me: {profileData.about_me}</p>
+//             </div>
+//         }
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
